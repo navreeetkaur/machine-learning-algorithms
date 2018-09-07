849b5a0c71328aaacf2936ae1167833652567d19
navreeetkaur <navreet700@gmail.com>
2018-09-08 01:24:27 +0530

    changed PCA

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,7 +4,7 @@ import operator
 import sys
 
 class PCA(object):
-	def __init__(self, retain_var, data, whiten = True, regularization = 10e-5):
+	def __init__(self, data, retain_var = 0.95, whiten = False, regularization = 10e-5):
 		self.retain_var = retain_var
 		self.whiten = whiten
 		self.regularization = regularization
@@ -25,18 +25,19 @@ class PCA(object):
 		Y = (Y.sum(axis=0))/(1.0*n)
 		# scaled data with zero mean and variance one
 		X = X/Y
+
 		return X
 
 
 	def reduce(self):
 	# compute covariance
-	cov_X = np.zeros(shape=(X.shape[1], X.shape[1]), dtype = np.float)
-	for i in range(X.shape[1]):
-		for j in range(i, X.shape[1]):
+	cov_X = np.zeros(shape=(self.X.shape[1], self.X.shape[1]), dtype = np.float)
+	for i in range(self.X.shape[1]):
+		for j in range(i, self.X.shape[1]):
 			sum = 0
-			for k in range(X.shape[0]):
-				sum += X[k][j]*X[k][i]
-			cov_X[i][j] = (sum*1.0)/(X.shape[0])
+			for k in range(self.X.shape[0]):
+				sum += self.X[k][j]* self.X[k][i]
+			cov_X[i][j] = (sum*1.0)/(self.X.shape[0])
 
 	# diagonalise covariance matrix to obtain eigenvalues
 	eigvals, eigvecs = np.linalg.eig(cov_X)
@@ -52,7 +53,7 @@ class PCA(object):
 	for k in range(1:len(eigvals)):
 		curr_sum += eigvals[k]
 		var_retained = curr_sum/(1.0*sum_n_eigvals)
-		if var_retained >= retain_var:
+		if var_retained >= self.retain_var:
 			break
 
 	self.U = eigvecs[:][:k+1] # (k x d)
@@ -60,25 +61,38 @@ class PCA(object):
 	Z = np.matmul(self.U, self.X.transpose()).transpose() # ((k x d) * (d x n)) = (n x k)
 	self.Z = Z
 	if self.whiten:
-		whiten()
+		self.W = whitening(self.Z)
 		return self.W
 	else:
 		return self.Z
 
 
-	def whiten(self):
-		# Whitening(optional) -  divide each feature by variance of that feature
-		mean_Z = self.Z.sum(axis=0)/(1.0*Z.shape[0])
-		zeromean_Z = self.Z - mean_Z
-		cov_Z = (np.matmul(zeromean_Z.transpose(), zeromean_Z))/(1.0 * self.Z.shape[0])
-		# regularisation added so that very small values do not blow up W 
-		var_Z = np.sqrt(cov_Z.diagonal() + self.regularization)
-		self.W = np.divide(self.Z, var_Z)
+	def reduce_test(self, test_X):
+		Z = np.matmul(self.U, test_X.transpose()).transpose() # ((k x d) * (d x n)) = (n x k)
+		if self.whiten:
+			return whitening(Z)
+		else:
+			return Z
 		
 
-	def retrieve(self):
+	def whitening(self, Z):
+		# Whitening(optional) -  divide each feature by variance of that feature
+		mean_Z = Z.sum(axis=0)/(1.0*Z.shape[0])
+		zeromean_Z = Z - mean_Z
+		cov_Z = (np.matmul(zeromean_Z.transpose(), zeromean_Z))/(1.0 * Z.shape[0])
+		# regularisation added so that very small values do not blow up W 
+		var_Z = np.sqrt(cov_Z.diagonal() + self.regularization)
+		W = np.divide(Z, var_Z)
+		return W
+
+
+	def retrieve(self, Z):
 		# to recover the original data
-		recover_X = np.matmul(self.U.transpose(), self.Z.transpose()).transpose()
+		if not whiten:
+			recover_X = np.matmul(self.U.transpose(), self.Z.transpose()).transpose()
+		else:
+			# recover_Z = 
+			recover_X = np.matmul(self.U.transpose(), recover_Z.transpose()).transpose()
 		return recover_X
 		
 
