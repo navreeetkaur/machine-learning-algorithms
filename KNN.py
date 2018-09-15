65a483c4ddb2f5eace0db44737318d94f08bf4c5
Utsiboy <34574922+Utsiboy@users.noreply.github.com>
2018-09-15 23:05:08 +0530

    KNN with distance wise labelling as parameter

--- KNN.py
+++ KNN.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class KNN:
-	def __init__(self,k,training_set,testing_set,names):
+	def __init__(self,k,training_set,testing_set,names,label_with_distance=False):
 		self.k = k
 		self.training_set = training_set
 		self.testing_set = testing_set
 		self.labels = np.zeros(len(testing_set))
 		self.names = names
+		self.label_with_distance = label_with_distance
 
 	def distance(self,data1,data2):
 		n = len(data1)
@@ -24,35 +25,75 @@ class KNN:
 		for i in range(0,n):
 			arr[0][i] = i
 			arr[1][i] = self.distance(data1,self.training_set[i])
-		# print(arr[1])
+		# print(arr[0])
 		ind = np.lexsort((arr[0],arr[1]))
-		arr1 = np.zeros(n)
+		arr1 = np.zeros((2,n))
 		for i in range(0,len(ind)):
-			arr1[i] = arr[0][ind[i]]
+			arr1[0][i] = arr[0][ind[i]]
+			arr1[1][i] = arr[1][ind[i]]
+		# print(arr1[:,0:k])
 		if(k>=n):
 			return arr1
 		else:
-			return arr1[0:k]
+			return arr1[:,0:k]
 
 	def labelling(self,data,position):
 		k = self.k
 		arr = self.sortedarr_k(data)
+		# print(arr)
 		arr1 = np.zeros(k)
 		for i in range(0,k):
-			arr1[i] = self.names[(int)(arr[i])]
+			arr1[i] = self.names[(int)(arr[0][i])]
+		# print(arr1)
 		u,ver  = np.unique(arr1, return_counts = True)
-		#print(u)
-		#print(ver)
+		# print(u)
+		# print(ver)
 		a = 0
 		for i in range(0,len(ver)):
 			if(ver[a]<ver[i]):
 				a = i
 		self.labels[position] = u[a]
 
+	def distance_wise_labelling(self,data,position):
+		k = self.k
+		arr = self.sortedarr_k(data)
+		# print(arr)
+		ind = np.lexsort((arr[1],arr[0]))
+		arr1 = np.zeros((2,k))
+		for i in range(0,k):
+			arr1[0][i] = arr[0][ind[i]]
+			arr1[1][i] = arr[1][ind[i]]
+		prev=0
+		maxsum = 0
+		maxclass = 0
+		while(prev<k):
+			cursum = 0 
+			curr = prev+1
+			while(curr<k and arr1[0][curr] == arr1[0][prev]):
+				if(arr1[1][prev]==0):
+					self.labels[position] = self.names[arr1[0][prev]]
+					return
+				cursum+=(1/(float)(arr1[1][prev]))
+				curr+=1
+				prev+=1
+			if(arr1[1][prev] == 0):
+				self.labels[position] = self.names[(int)(arr1[0][prev])]
+				return
+			cursum+=(1/(float)(arr1[1][prev]))
+			if(maxsum<cursum):
+				maxsum = cursum
+				maxclass = arr1[0][prev]
+			prev+=1
+		self.labels[position] = self.names[(int)(maxclass)]
+
+
 	def allocate(self):
 		testing_set = self.testing_set
 		for i in range(0, len(testing_set)):
-			self.labelling(testing_set[i],i)
+			if(self.label_with_distance):
+				self.distance_wise_labelling(testing_set[i],i)
+			else:
+				self.labelling(testing_set[i],i)
 
 
 def main():
@@ -61,12 +102,15 @@ def main():
 	file.close()
 	n = len(test_arr)
 	a = (int)(2*n/3)
-	test = test_arr[0:a,:-1]
-	training = test_arr[a:,:-1]
-	labels = test_arr[0:a,-1]
-	experiment = KNN(2,test,training,labels)
+	experiment = KNN(3,test_arr[:a,:-1],test_arr[a:,:-1],test_arr[:,-1])
 	experiment.allocate()
 	print(experiment.labels)
 
 if __name__=="__main__":
 	main()
+
+
+
+
+
+
