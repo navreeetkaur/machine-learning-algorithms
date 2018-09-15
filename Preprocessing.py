63163c6bdd0d9f38cc46c509f91b1347740960b7
navreeetkaur <navreet700@gmail.com>
2018-09-15 14:18:18 +0530

    removed noise from variance

--- Preprocessing.py
+++ Preprocessing.py
@@ -21,18 +21,21 @@ class Normalise(object):
 			X = X - means
 			# variance of each feature
 			Y = np.square(X)
+
 			# to account for zero variance
-			Y = np.add(Y, self.regularization)
+			# Y = np.add(Y, self.regularization)
+
 			Y = np.sqrt((Y.sum(axis=0))/(1.0*n))
 			self.scale_sigma = Y
 			# scaled data with zero mean and variance one
-			X = np.divide(X,Y)
+			X = np.divide(X, Y, out=np.zeros_like(X), where=Y!=0)
 			return X
+
 		elif len(self.scale_mean)!=0 and len(self.scale_sigma)!=0:
 			X = np.divide((X - self.scale_mean),(1.0*self.scale_sigma))
 			return X
+			
 		else:
-			print("Scaling parameters not computed. Scale training data first !!")
 			return
 
 
@@ -55,7 +58,6 @@ class PCA(object):
 
 	# feature scaling by Z-scoring 
 	def scale(self, X):
-		print("Need for scale")
 		d = X.shape[1] 
 		n = X.shape[0]
 		means = X.sum(axis=0)/(1.0*n)
@@ -64,8 +66,10 @@ class PCA(object):
 		X = X - means
 		# variance of each feature
 		Y = np.square(X)
-		# to account for zero variance
-		Y = np.add(Y, self.regularization)
+
+		# to account for zero variance - regularise
+		# Y = np.add(Y, self.regularization)
+
 		Y = np.sqrt((Y.sum(axis=0))/(1.0*n))
 		self.scale_sigma = Y
 		# scaled data with zero mean and variance one
@@ -75,7 +79,7 @@ class PCA(object):
 
 	def compute_eigen(self):
 		cov_X = (np.matmul(self.X.transpose(), self.X))/(1.0 * self.X.shape[0])
-		print("covariance computed. YAYAY")
+		# print("covariance computed. YAYAY")
 
 		# diagonalise covariance matrix to obtain eigenvalues
 		eigvals, eigvecs = np.linalg.eig(cov_X)
@@ -84,7 +88,7 @@ class PCA(object):
 		eig_sort = eigvals.argsort()
 		eigvals = eigvals[eig_sort[::-1]]
 		eigvecs = eigvecs[eig_sort[::-1]]
-		print("Eigenvalues and vectors computed. YAYAY")
+		# print("Eigenvalues and vectors computed. YAYAY")
 
 		# choose top k eigenvectors to form U - top k is determined by retaining 95% variance
 		sum_n_eigvals = eigvals.sum()
@@ -97,17 +101,18 @@ class PCA(object):
 
 		#U = eigvecs[:][:k+1] # (k x d)
 		self.k = k
+		print (f'Number of dimensions retained: [ {self.k} ]')
 		# rotated version of X in the space with eigenvector basis
 		self.x_rot = np.matmul(U.transpose(), self.X.transpose()).transpose() # (d x d, d x n).T  -> n x d
-		print("x_rot. YAYAY")
-		print("U computed. YAYAY")
+		# print("x_rot. YAYAY")
+		# print("U computed. YAYAY")
 		# U is the matrix of column eigenvectors in descending order of their corresponding eigenvalues
 		return U
 
 
 	def reduce(self, X, train):
 		if train:
-			print("Starting to reduce training data")
+			# print("Starting to reduce training data")
 			if self.whiten:
 				self.cov_x_rot = (np.matmul(self.x_rot.transpose(), self.x_rot))/(1.0 * self.x_rot.shape[0])
 				self.W = np.add(self.cov_x_rot.diagonal(), self.regularization)**(0.5)
@@ -117,7 +122,7 @@ class PCA(object):
 				Z = self.x_rot
 
 		else:
-			print("Starting to reduce testing data")		
+			# print("Starting to reduce testing data")		
 			# scale X with previous parameters
 			X = np.divide((X - self.scale_mean),(1.0*self.scale_sigma))
 			# rotate X
